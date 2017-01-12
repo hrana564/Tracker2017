388b888f5c9aa1ef17fc1fb4930c3a50810b4e23
hrana564 <hrana564@gmail.com>
2017-01-12 22:59:42 +0530

    Add files via upload

---
+++ models/activities.js
@@ -0,0 +1,9 @@
+﻿var mongoose = require('mongoose');
+
+var ActivitySchema = new mongoose.Schema({
+    ActivityName: String,
+    ActivitySelectorName : String,
+    ActivityActive: { type: Boolean, default: true }
+});
+
+module.exports = mongoose.model('Activities', ActivitySchema)
